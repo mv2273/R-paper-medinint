aea5cfe7447a63e13b4b3ffea229e5fad40f1e5b
mv2273 <139432693+mv2273@users.noreply.github.com>
2025-04-02 15:07:59 +0300

    Update effects_cc.R
    add code for optimising delta

--- effects_cc.R
+++ effects_cc.R
@@ -240,39 +240,19 @@ dat_taus <- cbind.data.frame(beta_ac0=unlist(beta_ac0),beta_ac1=unlist(beta_ac1)
                              tau_a0m0u0=unlist(tau_a0m0u0), tau_a1m1u1=unlist(tau_a1m1u1),
                              tau_a1m0ur=unlist(tau_a1m0ur))
 
-# joint probabilites of counterfactuals of U given sensitivity param rho
-pr_uj00 <- get_Ujoint(0)
-pr_uj25 <- get_Ujoint(.25)
-pr_uj50 <- get_Ujoint(.5)
-pr_uj75 <- get_Ujoint(.75)
-pr_uj100 <- get_Ujoint(1)
+# joint probabilites of counterfactuals of U with rho=0.5
 
-tau_mu_rho25 <- compute_rmst(wdat,M_args=list(gr=0,ref=F,score=factor(rep(0:2,each=3))),gr=1,score=factor(rep(0:2,3)),change=T,
-                             params=params,dat0=dat0,arg_list=argL,
-                             prU=pr_uj25%>%rename("score_m":=u0,"score":=u1),prW=pr_w)
+pr_uj50 <- get_Ujoint(.5)
+
+
 tau_mu_rho50 <- compute_rmst(wdat,M_args=list(gr=0,ref=F,score=factor(rep(0:2,each=3))),gr=1,score=factor(rep(0:2,3)),change=T,
                              params=params,dat0=dat0,arg_list=argL,
                              prU=pr_uj50%>%rename("score_m":=u0,"score":=u1),prW=pr_w)
-tau_mu_rho75 <- compute_rmst(wdat,M_args=list(gr=0,ref=F,score=factor(rep(0:2,each=3))),gr=1,score=factor(rep(0:2,3)),change=T,
-                             params=params,dat0=dat0,arg_list=argL,
-                             prU=pr_uj75%>%rename("score_m":=u0,"score":=u1),prW=pr_w)
-tau_mu_rho0 <- compute_rmst(wdat,M_args=list(gr=0,ref=F,score=factor(rep(0:2,each=3))),gr=1,score=factor(rep(0:2,3)),change=T,
-                            params=params,dat0=dat0,arg_list=argL,
-                             prU=pr_uj00%>%rename("score_m":=u0,"score":=u1),prW=pr_w)
-tau_mu_rho100 <- compute_rmst(wdat,M_args=list(gr=0,ref=F,score=factor(rep(0:2,each=3))),gr=1,score=factor(rep(0:2,3)),change=T,
-                              params=params,dat0=dat0,arg_list=argL,
-                             prU=pr_uj100%>%rename("score_m":=u0,"score":=u1),prW=pr_w)
 
-dat_taus <- dat_taus %>% mutate(tau_mu_rho0=unlist(tau_mu_rho0),
-                                tau_mu_rho25 = unlist(tau_mu_rho25),
-                                tau_mu_rho50 = unlist(tau_mu_rho50),
-                                tau_mu_rho75 = unlist(tau_mu_rho75),
-                                tau_mu_rho100 = unlist(tau_mu_rho100))
-delta_rho0 <- with(dat_taus,tau_mu_rho0 - tau_u1 - tau_a1m0ur + beta_ac1)
-delta_rho25 <- with(dat_taus,tau_mu_rho25 - tau_u1 - tau_a1m0ur + beta_ac1)
+
+dat_taus <- dat_taus %>% mutate( tau_mu_rho50 = unlist(tau_mu_rho50))
+
 delta_rho50 <- with(dat_taus,tau_mu_rho50 - tau_u1 - tau_a1m0ur + beta_ac1)
-delta_rho75 <- with(dat_taus,tau_mu_rho75 - tau_u1 - tau_a1m0ur + beta_ac1)
-delta_rho100 <- with(dat_taus,tau_mu_rho100 - tau_u1 - tau_a1m0ur + beta_ac1)
 
 Delta_DE <- with(dat_taus, tau_a0m0u0 - tau_u0 - tau_m00 + beta_ac0)
 Delta_IE <- with(dat_taus, tau_a1m1u1 - tau_u1 - tau_m11 + beta_ac1)
@@ -280,30 +260,101 @@ Delta_IE <- with(dat_taus, tau_a1m1u1 - tau_u1 - tau_m11 + beta_ac1)
 DE <- with(dat_taus, beta_ac0 - beta_ac1 + tau_u1 - tau_u0 +tau_m10-tau_m00)
 IE <- with(dat_taus, tau_m11 - tau_m10)
 
-eff_de_ie <- rbind(
-  data.frame(eff="IE",
-           assumption=c("rho0","rho25","rho50","rho75","rho100"),
-           rho = c(0,25,50,75,100),
-           mean = sapply(list(delta_rho0,delta_rho25,delta_rho50,delta_rho75,delta_rho100),
-                         function(xx)mean(IE+Delta_IE-xx)),
-           sd = sapply(list(delta_rho0,delta_rho25,delta_rho50,delta_rho75,delta_rho100),
-                       function(xx)sd(IE+Delta_IE-xx)),
-           q025 = sapply(list(delta_rho0,delta_rho25,delta_rho50,delta_rho75,delta_rho100),
-                         function(xx)as.numeric(quantile(IE+Delta_IE-xx,.025))),
-           q975 = sapply(list(delta_rho0,delta_rho25,delta_rho50,delta_rho75,delta_rho100),
-                         function(xx)as.numeric(quantile(IE+Delta_IE-xx,.975)))),
-  data.frame(eff="DE",
-           assumption=c("rho0","rho25","rho50","rho75","rho100"),
-           rho = c(0,25,50,75,100),
-           mean = sapply(list(delta_rho0,delta_rho25,delta_rho50,delta_rho75,delta_rho100),
-                         function(xx)mean(DE-Delta_DE+xx)),
-           sd = sapply(list(delta_rho0,delta_rho25,delta_rho50,delta_rho75,delta_rho100),
-                       function(xx)sd(DE-Delta_DE+xx)),
-           q025 = sapply(list(delta_rho0,delta_rho25,delta_rho50,delta_rho75,delta_rho100),
-                         function(xx)as.numeric(quantile(DE-Delta_DE+xx,.025))),
-           q975 = sapply(list(delta_rho0,delta_rho25,delta_rho50,delta_rho75,delta_rho100),
-                         function(xx)as.numeric(quantile(DE-Delta_DE+xx,.975)))))
+## min and max delta under monotonicity
+pr_uj00 <- get_Ujoint(0)
+pr_uj100 <- get_Ujoint(1)
 
+tau_mu_rho0_w <- compute_rmst(wdat,M_args=list(gr=0,ref=F,score=factor(rep(0:2,each=3))),gr=1,score=factor(rep(0:2,3)),change=T,
+                            params=params,dat0=dat0,arg_list=argL,
+                            prU=pr_uj00%>%rename("score_m":=u0,"score":=u1),prW=NULL)
+tau_mu_rho100_w <- compute_rmst(wdat,M_args=list(gr=0,ref=F,score=factor(rep(0:2,each=3))),gr=1,score=factor(rep(0:2,3)),change=T,
+                              params=params,dat0=dat0,arg_list=argL,
+                              prU=pr_uj100%>%rename("score_m":=u0,"score":=u1),prW=NULL)
+
+tau_0_100_arr <- abind::abind(list(tau_mu_rho0_w[,-1], tau_mu_rho100_w[,-1]),along=3)
+tau_minw <- apply(tau_0_100_arr, 1:2, min)
+tau_maxw <- apply(tau_0_100_arr, 1:2, max)
+
+delta_min <- apply(tau_minw*pr_w[,-1],2,sum) - taus$tau_u1 - taus$tau_a1m0ur + taus$beta_ac1
+delta_max <- apply(tau_maxw*pr_w[,-1],2,sum) - taus$tau_u1 - taus$tau_a1m0ur + taus$beta_ac1
+de_min_mono <- DE - Delta_DE + delta_min
+de_max_mono <- DE - Delta_DE + delta_max
+de_50 <- DE - Delta_DE + delta_rho50
+ie_min_mono <- IE + Delta_IE - delta_max
+ie_max_mono <- IE + Delta_IE - delta_min
+ie_50 <- IE + Delta_IE - delta_rho50
+
+## under free joint probability matrix (rerun relevant previous code without removing the iterations not compatible with monotonicity)
+tau_mu <- compute_rmst(wdat,M_args=list(gr=0,ref=F,score=factor(rep(0:2,each=3))),gr=1,score=factor(rep(0:2,3)),change=T,
+                              params=params,dat0=dat0,arg_list=argL,
+                              prU=NULL,prW=NULL)
+
+pr_U_long <- pr_U %>% pivot_longer(cols=starts_with("iter"), names_to="iter",names_pattern="iter_([0-9]*)",values_to="val_U") %>%
+  select(w_strata,score,gr,iter,val_U) %>%mutate(iter = as.numeric(iter))%>% arrange(w_strata,iter,score,gr)%>%
+  pivot_wider(id_cols=c(w_strata,iter,score), values_from="val_U", names_from="gr",names_prefix="pr_")
+
+tau_mu_long <- tau_mu %>% pivot_longer(cols=starts_with("iter_"), 
+                                       values_to="val",names_to="iter", names_pattern="iter_([0-9]*)") %>%
+  mutate(iter=as.numeric(iter)) %>%
+  arrange(w_strata,iter,score,score_m) %>% rename("u0"=score_m, "u1"=score) %>%
+  left_join(pr_U_long %>% select(w_strata,iter,"u1"=score,"phi"=pr_1)) %>%
+  left_join(pr_U_long %>% select(w_strata,iter,"u0"=score,"psi"=pr_0))
+
+# nine rows per stratum--iteration combination
+inds <- cbind(c(seq(1, nrow(tau_mu_long), 9)),
+              seq(9,nrow(tau_mu_long),9))
+
+constraint <- with(tau_mu_long[inds[1,1]:inds[1,2],],
+                   rbind(u1==0&u0==1, u1==0&u0==2, u1==1&u0==1, u1==1&u0==2,
+                         u1==0&u0==1, u1==0&u0==2, u1==1&u0==1, u1==1&u0==2,
+                         u0==1, u0==2, u1==0, u1==1,
+                         1))
+dirs <- c(rep(">=",4), rep("<=",4), rep("==",5))
+
+# indices needed in the loop below
+p_inds <- with(tau_mu_long[inds[1,1]:inds[1,2],],
+               c(which(u1==0&u0==1),which(u1==0&u0==2), which(u1==1&u0==1), which(u1==1&u0==2)))
+
+pr_min <- vector("double", nrow(tau_mu_long))
+pr_max <- vector("double",nrow(tau_mu_long))
+
+for(i in 1:nrow(inds)){
+  tau_tmp <- tau_mu_long[inds[i,1]:inds[i,2],]
+  bb <- tau_tmp[["val"]]
+  rhs <- with(tau_tmp, c(pmax(0,phi[p_inds]+psi[p_inds]-1), #alarajat
+                         pmin(phi,psi)[p_inds], #ylärajat
+                         psi[u0==1&u1==0],
+                         psi[u0==2&u1==0],
+                         phi[u1==0&u0==0],
+                         phi[u1==1&u0==0],
+                         1))
+  lpmin <- lpSolve::lp("min",bb,constraint,dirs,rhs)$solution
+  lpmax <- lpSolve::lp("max",bb,constraint,dirs,rhs)$solution
+  pr_min[inds[i,1]:inds[i,2]] <- lpmin
+  pr_max[inds[i,1]:inds[i,2]] <- lpmax
+}
+tau_mu_long <- tau_mu_long %>% mutate(pr_min = pr_min, pr_max=pr_max)
+pr_umax <- tau_mu_long %>% select(w_strata,u1,u0,iter,pr_max) %>%
+  pivot_wider(id_cols=c(w_strata,u1,u0), names_from="iter", names_prefix = "iter_", values_from=pr_max)
+pr_umin <- tau_mu_long %>% select(w_strata,u1,u0,iter,pr_min) %>%
+  pivot_wider(id_cols=c(w_strata,u1,u0), names_from="iter", names_prefix = "iter_", values_from=pr_min)
+tau_mu_wide <- tau_mu_long %>% select(w_strata,u1,u0,iter,val) %>%
+  pivot_wider(id_cols=c(w_strata,u1,u0), names_from="iter", names_prefix = "iter_", values_from=val)
+pr_w_minmax <- tau_mu_wide %>% select(w_strata) %>% left_join(pr_w)
+
+
+tau_mu_min <- apply(tau_mu_wide[,-c(1:3)]*pr_umin[,-c(1:3)]*pr_w_minmax[,-c(1)],2,sum)
+tau_mu_max <- apply(tau_mu_wide[,-c(1:3)]*pr_umax[,-c(1:3)]*pr_w_minmax[,-c(1)],2,sum)
+delta_min_free <- with(dat_taus, tau_mu_min - tau_u1 - tau_a1m0ur + beta_ac1)
+delta_max_free <- with(dat_taus, tau_mu_max - tau_u1 - tau_a1m0ur + beta_ac1)
+
+# Effects under relaxing monotonicity:
+de_min_free <- DE - Delta_DE + delta_min_free; de_max_free <- DE - Delta_DE + delta_max_free,
+ie_min_free <- IE + Delta_IE - delta_max_free; ie_max_free <- IE + Delta_IE - delta_min_free)
+
+
+
+## TE
 te_nonp <- survRM2::rmst2(surv_dat$stop_time,surv_dat$status,surv_dat$gr, tau=15)$unadj[1,]
 te <- with(taus,tau_a1m1u1-tau_a0m0u0)
 te_summ <- c("Est"=mean(te),"sd"=sd(te),"median"=median(te),quantile(te,c(.025,.975)))
